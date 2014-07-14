01c245f5ed958734afbb55a65a46515ee68b4ac3
William Willing <william@williamwilling.com>
2014-07-14 22:02:34 +0200

    You can now create a board in table storage.

--- src/WilliamWilling.Ginger/Controllers/GoController.cs
+++ src/WilliamWilling.Ginger/Controllers/GoController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using WilliamWilling.Ginger.Models;
 
 namespace WilliamWilling.Ginger.Controllers
 {
@@ -17,5 +21,45 @@ namespace WilliamWilling.Ginger.Controllers
         {
             return View();
         }
+
+        public ActionResult Create()
+        {
+            return RedirectToAction("Details", new { id = CreateBoard() });
+        }
+
+        private string CreateBoard()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["TableStorage"].ConnectionString;
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+            var client = storageAccount.CreateCloudTableClient();
+            var table = client.GetTableReference("ginger");
+            table.CreateIfNotExists();
+
+            bool created = false;
+            BoardEntity board = new BoardEntity("go");
+
+            while (!created)
+            {
+                try
+                {
+                    var operation = TableOperation.Insert(board);
+                    table.Execute(operation);
+                    created = true;
+                }
+                catch (StorageException e)
+                {
+                    if (e.RequestInformation.HttpStatusCode != 409)
+                    {
+                        throw;
+                    }
+
+                    board = new BoardEntity("go");
+                }
+            }
+
+            return board.RowKey;
+        }
+
+        
 	}
 }
---
+++ src/WilliamWilling.Ginger/Models/BoardEntity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace WilliamWilling.Ginger.Models
+{
+    public class BoardEntity : TableEntity
+    {
+        public BoardEntity()
+        {
+        }
+
+        public BoardEntity(string game)
+        {
+            PartitionKey = game;
+            RowKey = GenerateId();
+            Version = 1;
+        }
+
+        public int Version
+        {
+            get;
+            set;
+        }
+
+        public string Contents
+        {
+            get;
+            set;
+        }
+
+        private string GenerateId()
+        {
+            char[] consonants = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'w', 'z' };
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            StringBuilder id = new StringBuilder(8);
+            var random = new Random();
+
+            for (int i = 0; i < 4; i++)
+            {
+                id.Append(consonants[random.Next(consonants.Length)]);
+                id.Append(vowels[random.Next(vowels.Length)]);
+            }
+
+            return id.ToString();
+        }
+    }
+}
